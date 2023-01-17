d0bbb663858e7fe6507b093f05089c4ae26cc762
Salim Mouttaki <MOU@inokscapital.com>
2023-01-17 14:53:41 +0100

    added a command to create a new State

---
+++ ComputerStockApi/Commands/State/CreateStateCommand.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ComputerStockApi.Data;
+using ComputerStockApi.Dtos;
+using ComputerStockApi.Models;
+using MediatR;
+
+namespace ComputerStockApi.Commands.State
+{
+    public class CreateStateCommand : StateDto, IRequest
+    {
+    }
+    public class CreateStateCommandHandler : IRequestHandler<CreateStateCommand>
+    {
+        private readonly IMapper mapper;
+        private readonly ComputerStockContext _context;
+
+        public CreateStateCommandHandler(IMapper mapper, IConfiguration config)
+        {
+            this.mapper = mapper;
+            _context = new ComputerStockContext(config);
+        }
+
+        public async Task<Unit> Handle(CreateStateCommand command, CancellationToken cancellationToken)
+        {
+            var dao = mapper.Map<StateDao>(command);
+
+            await _context.State.AddAsync(dao);
+
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+
+     }
+}
