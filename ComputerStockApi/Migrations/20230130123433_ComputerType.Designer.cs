54c764b89186c46a6007f595afdfb8d13160032f
Salim Mouttaki <MOU@inokscapital.com>
2023-01-30 13:40:08 +0100

    migration for seed

--- ComputerStockApi/Migrations/20230130123433_ComputerType.Designer.cs
+++ ComputerStockApi/Migrations/20230130123433_ComputerType.Designer.cs
@@ -4,6 +4,7 @@ using ComputerStockApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ComputerStockApi.Migrations
 {
     [DbContext(typeof(ComputerStockContext))]
-    partial class ComputerStockContextModelSnapshot : ModelSnapshot
+    [Migration("20230130123433_ComputerType")]
+    partial class ComputerType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ComputerStockApi/Migrations/20230130123433_ComputerType.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ComputerStockApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComputerType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ComputerType",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Laptop" },
+                    { 2, "Mini-Computer" },
+                    { 3, "Fix" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ComputerType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ComputerType",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ComputerType",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
